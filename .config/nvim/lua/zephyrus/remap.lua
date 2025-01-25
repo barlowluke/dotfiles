7c92d08100e78235eb47f3b613d83c35e85759db
luke <barlowlukew@gmail.com>
2025-01-24 20:53:56 -0500

    rephrase remap commands

--- .config/nvim/lua/zephyrus/remap.lua
+++ .config/nvim/lua/zephyrus/remap.lua
@@ -1,3 +1,3 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>hc", vim.cmd.let @/ = "")
+vim.keymap.set("n", "<Leader>pv", "<cmd>Ex<CR>")
+vim.keymap.set("n", "<Leader>hc", "<cmd>let @/ = ''<CR>")
