ae353b1a240652553861a564cec7369d96904460
luke <barlowlukew@gmail.com>
2025-01-26 20:52:37 -0500

    add my vim buffer remaps to nvim

--- .config/nvim/lua/eurus/remap.lua
+++ .config/nvim/lua/eurus/remap.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<Leader>pv", "<cmd>Ex<CR>")
 vim.keymap.set("n", "<Leader>hc", "<cmd>let @/ = ''<CR>")
+vim.keymap.set("n", "<Leader>n", "<cmd>bnext<CR>")
+vim.keymap.set("n", "<Leader>p", "<cmd>bprevious<CR>")
+vim.keymap.set("n", "<Leader>d", "<cmd>bdelete<CR>")
