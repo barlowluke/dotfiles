cb5a4aae12055d5fdbbb92a9159a8e42bf2b833f
luke <barlowlukew@gmail.com>
2025-07-01 14:41:22 -0400

    merge config.el

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -38,6 +38,10 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
 
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
